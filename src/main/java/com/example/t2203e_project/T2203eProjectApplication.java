58da228a7151a81ecb77da9f19f17458819b9be2
GraviousDante <toankhanh2003@gmail.com>
2023-11-14 18:42:47 +0700

    Project

---
+++ src/main/java/com/example/t2203e_project/T2203eProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.t2203e_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2203eProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T2203eProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/t2203e_project/T2203eProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.t2203e_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2203eProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
